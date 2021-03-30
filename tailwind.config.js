0605942f63ce103b88f0dd3a175ea991cf9f5552
Jason Cockerham <jason.cockerham@gmail.com>
2021-03-30 08:14:22 -0400

    Updated purge css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 // See https://tailwindcss.com/docs/configuration for details
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+	purge: [
+    "./public/**/*.html",
+    "./src/**/*.{js,jsx,ts,tsx,vue}",
+  ],
   // https://github.com/tailwindlabs/tailwindcss-forms
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
