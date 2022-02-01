1e14eda39407869a6d9edf0ea9332f8f6c699565
Jason Cockerham <jason.cockerham@gmail.com>
2022-02-01 12:02:08 -0500

    Added webpack config item to allow local testing in browserstack

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,16 @@
 const path = require('path');
 
+// BROWSERSTACK:
+// Hostname restrictions in local server configuration
+// https://www.browserstack.com/docs/live/local-testing/ios-troubleshooting-guide#hostname-restrictions-in-local-server-configuration
+
 // Absolute imports
 // https://www.gatsbyjs.com/docs/how-to/custom-configuration/add-custom-webpack-config/#absolute-imports
 exports.onCreateWebpackConfig = ({ actions }) => {
 	actions.setWebpackConfig({
+		devServer: {
+			disableHostCheck: true
+		},
 		resolve: {
 			alias: {
 				"~/data": path.resolve(__dirname, 'data'),
