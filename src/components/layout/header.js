27ccb726f980f23434075375f19dd33e9e6d6ae7
Jason Cockerham <jason.cockerham@gmail.com>
2022-03-22 11:14:57 -0400

    Refactored nav and header; created routes file

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,26 +1,15 @@
 import React, { useState } from 'react';
-import { graphql, useStaticQuery, Link } from 'gatsby';
 import Headroom from 'react-headroom';
-import { Icon } from '@iconify/react';
+
+import Navbar from '~/components/modules/navigation/navbar';
 
 const Header = () => {
-	const [isExpanded, toggleExpansion] = useState(false);
 	const [position, setPosition] = useState(false);
 
 	const PosMap = {
 		PINNED: 'PINNED',
 		DEFAULT: 'DEFAULT'
 	};
-
-	const { site } = useStaticQuery(graphql`
-		query SiteTitleQuery {
-			site {
-				siteMetadata {
-					title
-				}
-			}
-		}
-	`);
 
 	return (
 		<Headroom
@@ -29,59 +18,7 @@ const Header = () => {
 			onUnfix={() => setPosition(PosMap.DEFAULT)}
 		>
 			<header className="bg-gray-200 shadow-lg">
-				<div className={`flex flex-wrap items-center justify-between container mx-auto p-4 ${position === PosMap.PINNED ? 'md:py-4' : 'md:py-6'}`}>
-					<Link
-						to="/"
-						className="flex items-center"
-					>
-						<Icon
-							icon="mdi:firefox"
-							className={`text-indigo-700 mr-1 ${position === PosMap.PINNED ? 'w-10 h-10' : 'w-12 h-12'}`}
-						/>
-						<h1 className={`text-gray-800 font-bold no-underline ${position === PosMap.PINNED ? 'text-lg' : 'text-2xl'}`}>
-							{site.siteMetadata.title}
-						</h1>
-					</Link>
-
-					<button
-						className="items-center block px-3 py-2 text-white border border-white rounded md:hidden"
-						onClick={() => toggleExpansion(!isExpanded)}
-					>
-						<svg
-							className="w-3 h-3 fill-current"
-							viewBox="0 0 20 20"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<title>Menu</title>
-							<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-						</svg>
-					</button>
-
-					<nav
-						className={`${
-							isExpanded ? `block` : `hidden`
-						} md:block md:items-center w-full md:w-auto`}
-					>
-						{[
-							{
-								route: `/about`,
-								title: `About`
-							},
-							{
-								route: `/contact`,
-								title: `Contact`
-							}
-						].map(link => (
-							<Link
-								className="block mt-4 text-gray-800 no-underline md:inline-block md:mt-0 md:ml-6 border-b-2 border-transparent hover:text-indigo-700 transition-colors"
-								key={link.title}
-								to={link.route}
-							>
-								{link.title}
-							</Link>
-						))}
-					</nav>
-				</div>
+				<Navbar position={position} />
 			</header>
 		</Headroom>
 	);
---
+++ src/components/modules/navigation/navbar.js
@@ -0,0 +1,114 @@
+import React, { useState } from 'react';
+import { graphql, useStaticQuery, Link } from 'gatsby';
+import { Icon } from '@iconify/react';
+
+import routes from '~/data/routes.json';
+
+const Navbar = ({ position }) => {
+	const [isExpanded, toggleExpansion] = useState(false);
+
+	const { site } = useStaticQuery(graphql`
+		query SiteTitleQuery {
+			site {
+				siteMetadata {
+					title
+				}
+			}
+		}
+	`);
+
+	return (
+		<div
+			className={`
+				flex flex-wrap items-center justify-between container mx-auto p-4
+				${position === 'PINNED' ? 'md:py-4' : 'md:py-6'}
+			`}
+		>
+			<Link
+				to="/"
+				className="flex items-center"
+			>
+				<Icon
+					icon="mdi:firefox"
+					className={`
+						text-indigo-700 mr-1
+						${position === 'PINNED' ? 'w-10 h-10' : 'w-12 h-12'}
+					`}
+				/>
+				<h1
+					className={`
+						text-gray-800 font-bold no-underline
+						${position === 'PINNED' ? 'text-lg' : 'text-2xl'}
+					`}
+				>
+					{site.siteMetadata.title}
+				</h1>
+			</Link>
+
+			<button
+				className={`
+					block md:hidden px-3 py-2 transition-colors hover:text-indigo-700 focus:text-indigo-700
+					${isExpanded ? 'text-indigo-700' : 'text-gray-800'}
+				`}
+				onClick={() => toggleExpansion(!isExpanded)}
+			>
+				<svg
+					viewBox="0 0 20 20"
+					xmlns="http://www.w3.org/2000/svg"
+					className={`
+						block w-5 h-5 fill-current transition-transform
+						${isExpanded ? `transform-gpu rotate-180` : ``}
+					`}
+				>
+					<title>Menu</title>
+					<rect
+						y="3"
+						width="20"
+						height="2"
+						className={`
+							transition
+							${isExpanded ? `transform-gpu rotate-45 translate-y-[0] translate-x-[6px]` : ``}
+						`}
+					/>
+					<rect
+						y="9"
+						width="20"
+						height="2"
+						className={`
+							transition
+							${isExpanded ? `opacity-0` : ``}
+						`}
+					/>
+					<rect
+						y="15"
+						width="20"
+						height="2"
+						className={`
+							transition
+							${isExpanded ? `transform-gpu -rotate-45 translate-y-[6px] translate-x-[-8px]` : ``}
+						`}
+					/>
+				</svg>
+			</button>
+
+			<nav
+				className={`
+					md:flex md:items-center md:gap-6 w-full md:w-auto pt-2 md:pt-0
+					${isExpanded ? 'block' : 'hidden'}
+				`}
+			>
+				{routes.map(link => (
+					<Link
+						className="block md:inline-block px-4 py-2 md:p-2 text-gray-800 no-underline border-b-2 border-transparent transition-colors hover:text-indigo-700 focus:text-indigo-700"
+						key={link.title}
+						to={link.route}
+					>
+						{link.title}
+					</Link>
+				))}
+			</nav>
+		</div>
+	);
+};
+
+export default Navbar;
