57314b9c392010d9c25ca40bc9d083b8bc9deb1f
Jason Cockerham <jason.cockerham@gmail.com>
2022-05-24 09:31:46 -0400

    Updated button component

--- src/components/modules/button.js
+++ src/components/modules/button.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { Link } from 'gatsby';
+import { Icon } from '@iconify/react';
+import * as Scroll from 'react-scroll';
 
-const Button = ({ as = 'link', to, type, size, variant, children, className, ...rest }) => {
+const IconPlacement = Object.freeze({
+	BEFORE: 'before',
+	AFTER: 'after'
+});
+
+const Button = forwardRef((props, ref) => {
+	const {
+		as = 'link',
+		to,
+		type,
+		size,
+		variant,
+		children,
+		className,
+		buttonIconContainerClass,
+		icon,
+		iconPlacement = 'after',
+		iconClassName,
+		hasUnderline,
+		...rest
+	} = props;
+
+	let ScrollLink = Scroll.Link;
+
 	const Defaults = {
 		size: 'py-2 px-6',
 		variant: 'inline-block font-bold uppercase text-center rounded transition-colors',
@@ -10,13 +35,54 @@ const Button = ({ as = 'link', to, type, size, variant, children, className, ...
 
 	const ButtonVariant = {
 		primary: 'bg-blue-600 text-white hover:bg-blue-800 focus:bg-blue-800',
-		secondary: 'bg-pink-600 text-white hover:bg-pink-800 focus:bg-pink-800'
+		secondary: 'bg-pink-600 text-white hover:bg-pink-800 focus:bg-pink-800',
+		link: `normal-case text-blue-600 font-normal [font-size:inherit] !p-0 ${hasUnderline ? underlineClasses : ''}`
 	};
 
 	const ButtonSize = {
 		sm: 'py-1 px-4 text-sm',
 		lg: 'py-4 px-10 text-lg'
 	};
+
+	const underlineClasses = "!underline hover:!no-underline focus:!no-underline";
+
+	const linkVariant = variant === 'link';
+
+	const ButtonIcon = <Icon
+		icon={icon}
+		className={`
+			${linkVariant ? 'w-3.5 h-3.5' : 'w-4 h-4'}
+			${iconClassName ?? ''}
+		`}
+	/>;
+
+	const ButtonBody =
+		<>
+			{icon ?
+				<span className={`
+					inline-flex items-center justify-between text-left
+					${linkVariant ? 'inline-flex' : 'flex'}
+					${buttonIconContainerClass ?? ''}
+				`}>
+					{iconPlacement === IconPlacement.BEFORE &&
+						ButtonIcon
+					}
+					<span
+						className={`
+							flex-1
+							${iconPlacement === IconPlacement.BEFORE ? 'pl-1.5' : 'pr-1.5'}
+						`}
+					>
+						{children}
+					</span>
+					{iconPlacement === IconPlacement.AFTER &&
+						ButtonIcon
+					}
+				</span>
+				:
+				children
+			}
+		</>;
 
 	switch (as.toLowerCase()) {
 	case 'a':
@@ -33,12 +99,13 @@ const Button = ({ as = 'link', to, type, size, variant, children, className, ...
 				`}
 				{...rest}
 			>
-				{children}
+				{ButtonBody}
 			</a>
 		);
 	case 'button':
 		return (
 			<button
+				ref={ref}
 				type={type ? type : Defaults.type}
 				className={`
 					${size ? ButtonSize[size] : Defaults.size}
@@ -48,8 +115,27 @@ const Button = ({ as = 'link', to, type, size, variant, children, className, ...
 				`}
 				{...rest}
 			>
-				{children}
+				{ButtonBody}
 			</button>
+		);
+	case 'scroll':
+		return (
+			<ScrollLink
+				href={`#${to}`}
+				to={to}
+				smooth={true}
+				offset={-25}
+				duration={250}
+				className={`
+					${size ? ButtonSize[size] : Defaults.size}
+					${Defaults.variant}
+					${variant ? ButtonVariant[variant] : ''}
+					${className ?? ''}
+				`}
+				{...rest}
+			>
+				{ButtonBody}
+			</ScrollLink>
 		);
 	default:
 		return (
@@ -63,10 +149,11 @@ const Button = ({ as = 'link', to, type, size, variant, children, className, ...
 				`}
 				{...rest}
 			>
-				{children}
+				{ButtonBody}
 			</Link>
 		);
 	}
-};
+});
 
+Button.displayName = 'Button';
 export default Button;
