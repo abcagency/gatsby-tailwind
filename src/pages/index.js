418b3596dc9fdaf9a498707efc69af7c78a70270
jcock <jason.cockerham@gmail.com>
2023-04-05 10:39:22 -0400

    Added generic card component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from '~/components/layout/layout';
 import PageHead from '~/components/util/page-head';
 import PageNav from '~/components/modules/navigation/page';
 import Section from '~/components/modules/section';
+import Card from '~/components/modules/card';
 
 import useSectionTracker from '~/hooks/useSectionTracker';
 
@@ -44,6 +45,30 @@ const IndexPage = ({ location }) => {
 				setCurrentSection={setCurrentSection}
 				className="container px-4 my-12"
 			>
+				<Card
+					className="text-center max-w-xs mx-auto mb-12"
+					image={
+						<Card.Image
+							image="https://picsum.photos/id/237/640/480"
+							alt="Dog"
+						/>
+					}
+					info={
+						<Card.Info>
+							<Card.Title className="mb-2">Catchy title</Card.Title>
+							<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat dolor inventore deserunt, perferendis asperiores.</p>
+						</Card.Info>
+					}
+					footer={
+						<Card.Footer>
+							<Card.Button
+								onClick={console.log('navigated')}
+							>
+								Check it out
+							</Card.Button>
+						</Card.Footer>
+					}
+				/>
 				<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat dolor inventore deserunt, perferendis asperiores quibusdam repudiandae, sequi maiores unde tempora esse aliquid necessitatibus, excepturi ea incidunt ex blanditiis tenetur beatae. Fugiat voluptatem blanditiis reiciendis earum repellat, qui dolor tenetur maiores at voluptate enim modi asperiores ab corrupti explicabo recusandae ea excepturi assumenda quae iure rem similique consectetur veritatis minima. Natus. Ipsa aliquam sapiente expedita quidem vitae sit architecto eveniet est id quaerat saepe modi unde esse sequi sint dolorem adipisci in eos, ducimus reprehenderit ea, iusto molestiae rem. Ad, ipsam. A quisquam ex dolor pariatur debitis sequi veritatis rem delectus facere totam, incidunt quibusdam fugiat provident repudiandae laborum mollitia dolorum odit? Molestiae alias, temporibus culpa fugiat libero incidunt perspiciatis voluptas. Quia, necessitatibus! Ipsam accusamus atque ipsum nisi dolor repudiandae officiis, cum accusantium iure inventore facilis magni debitis itaque blanditiis, placeat eum? Cupiditate error obcaecati soluta consectetur, in officiis quaerat aliquid! Rerum ut odio dolore excepturi ipsam ducimus sapiente quas consequuntur porro iure optio laborum, voluptas doloribus id rem tenetur eligendi delectus corporis aspernatur amet necessitatibus! Numquam officia sunt maxime nihil? Iure, asperiores beatae amet odit, autem, quidem id officia maxime debitis rerum unde distinctio. Ipsa, dolore quidem quibusdam nihil repellendus sapiente recusandae corrupti deserunt dolor ad a officia quis odio. Rem, natus sapiente! Modi enim quidem consequatur, nobis facere eligendi similique vero rerum praesentium fuga nesciunt, nihil velit. Repudiandae atque illo repellendus sunt nostrum nobis rerum soluta eos praesentium! Autem! Unde odio vero, nesciunt voluptas eius sit. Quia, ex earum beatae mollitia ipsum at, aliquam officiis cum minus molestiae error quos voluptatibus. Ipsam quod rem fugiat, voluptatum voluptas quasi non!  Recusandae ea excepturi dignissimos vel nisi voluptatum inventore sapiente est facilis, at modi provident, eius eligendi placeat error architecto odio? Explicabo soluta, culpa qui blanditiis alias officiis dolore eaque expedita?</p>
 			</Section>
 
